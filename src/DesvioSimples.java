91b71957e74801e4c499dd64cce4dd026289a5db
Sara Rosalem <saraanunex@gmail.com>
2021-02-24 22:11:58 -0300

    Adding agee validation

--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -4,15 +4,23 @@ public class DesvioSimples {
 
 	public static void main(String[] args) {
 		/*Uma ONG criou um jogo para alertar as pessoas sobre os riscos das drogas.
-		 *Por se tratar de um assunto sensível, ficou decidido que a idade mínima para jogar é de 12 anos.
-		 *Sua função é criar um programa que receba a idade do usuário e exiba a mensagem "Você pode jogar" caso ele tenha 12 anos ou mais. */
+		 *Por se tratar de um assunto sensï¿½vel, ficou decidido que a idade mï¿½nima para jogar ï¿½ de 12 anos.
+		 *Sua funï¿½ï¿½o ï¿½ criar um programa que receba a idade do usuï¿½rio e exiba a mensagem "Vocï¿½ pode jogar" caso ele tenha 12 anos ou mais. */
 		
 		Scanner leitor = new Scanner(System.in);
 		int idade;
 		System.out.println("Por favor, digite a sua idade!");
 		idade = leitor.nextInt();
 		System.out.println("Deixe-me verificar sua idade...");
-		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
+		/*Aqui devemos criar uma soluï¿½ï¿½o para verificar a idade do usuï¿½rio*/
+		
+		// if significa SE + (condicao) for verdadeira, execute tal coisa, 
+		if (idade>=12) {
+			System.out.println("Voce pode jogar");
+		
+		//caso contrario execute outra acao	
+		}else
+			System.out.println("Voce nao esta autorizado");
 		
 		leitor.close();
 
